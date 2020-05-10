d88824b977b0d3105011d2570206e4c18c743f98
Will Reichl <William.Reichl@dcsg.com>
2020-05-09 22:58:45 -0400

    Decorator exercise solution

--- Decorator.Exercise/Exercise.cs
+++ Decorator.Exercise/Exercise.cs
@@ -2,47 +2,63 @@
 
 namespace Decorator.Exercise
 {
-  /**
-   * The following code scenario shows a Dragon which is both a Bird and a Lizard.
-   *
-   * Complete the Dragon's interface (there's no need to extract IBird or ILizard). 
-   * Take special care when implementing the Age property.
-   */
+    /**
+     * The following code scenario shows a Dragon which is both a Bird and a Lizard.
+     *
+     * Complete the Dragon's interface (there's no need to extract IBird or ILizard). 
+     * Take special care when implementing the Age property.
+     */
     public class Bird
     {
-      public int Age { get; set; }
-      
-      public string Fly()
-      {
-        return (Age < 10) ? "flying" : "too old";
-      }
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
     }
 
     public class Lizard
     {
-      public int Age { get; set; }
-      
-      public string Crawl()
-      {
-        return (Age > 1) ? "crawling" : "too young";
-      }
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
     }
 
     public class Dragon // no need for interfaces
     {
-      public int Age
-      {
-        // todo :)
-      }
+        private Lizard lizard;
+        private Bird bird;
 
-      public string Fly()
-      {
-        // todo
-      }
+        public Dragon()
+        {
+            this.lizard = new Lizard();
+            this.bird = new Bird();
+        }
 
-      public string Crawl()
-      {
-        // todo
-      }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set 
+            {
+                _age = value;
+                bird.Age = value;
+                lizard.Age = value;
+            }
+        }
+
+        public string Fly()
+        {
+            return bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return lizard.Crawl();
+        }
     }
 }
